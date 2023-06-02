af9512f7949586cc9d17a94de292182aaca5f832
李* <jiandinglixiang@qq.com>
2023-06-02 21:21:51 +0800

    init

--- pages/guide/index.vue
+++ pages/guide/index.vue
@@ -4,7 +4,12 @@
       <image class="guide-image" src="/static/logo.png"></image>
       <text class="guide-text">注册会计师App</text>
     </view>
-    <popup-index ref="privacyPopup" @action="handleAction" :popup-key="PRIVACY_AUTH_POPUP" />
+    <popup-index
+      v-if="!ConfirmAuthorization"
+      ref="privacyPopup"
+      @action="handleAction"
+      :popup-key="PRIVACY_AUTH_POPUP"
+    />
   </view>
 </template>
 
@@ -12,34 +17,35 @@
 import { PRIVACY_AUTH_POPUP } from "@/components/popup/popupKeyMap"
 import PopupIndex from "@/components/popup/PopupIndex.vue"
 import { userInfo } from "@/pinia/user"
-import { onLoad, onUnload } from "@dcloudio/uni-app"
+import { CONFIRM_AUTHORIZATION } from "@/utils/consts"
+import { onLoad } from "@dcloudio/uni-app"
 import { nextTick, ref } from "vue"
 import { AppAuditStatus } from "@/pinia/audit"
-const userInfoStore = userInfo()
+
+// 是否授权
+const ConfirmAuthorization = uni.getStorageSync(CONFIRM_AUTHORIZATION)
 const privacyPopup = ref()
-onLoad(() => {
+const auditStatus = AppAuditStatus().getAppAuditStatus()
+
+onLoad(async () => {
   //#ifdef APP-PLUS
   // plus.navigator.setFullscreen(true)
   plus.navigator.closeSplashscreen() // 关闭启动页
   //#endif
+  await nextTick()
+  if (ConfirmAuthorization) {
+    goNext()
+  } else {
+    privacyPopup.value.open()
+  }
 })
-nextTick(() => {
-  privacyPopup.value.open()
-})
-//#ifdef APP-PLUS
-// onUnload(() => {
-// plus.navigator.setFullscreen(false)
-// })
-//#endif
-const auditStatus = AppAuditStatus().getAppAuditStatus()
 
 async function goNext() {
   try {
-    privacyPopup.value.close()
     const { auditStatusBoolean } = await auditStatus
-    if (!import.meta.env.DEV && auditStatusBoolean) {
+    if (auditStatusBoolean) {
       uni.reLaunch({ url: "/pages/home/index" })
-    } else if (await userInfoStore.getUserInfo()) {
+    } else if (await userInfo().getUserInfo()) {
       uni.reLaunch({ url: `/pages/user/index` })
     } else {
       uni.reLaunch({ url: `/pages/login/index` })
@@ -52,6 +58,7 @@ async function goNext() {
 
 function handleAction(action) {
   if (action === "agree") {
+    privacyPopup.value.close()
     goNext()
   } else {
     // #ifdef APP-PLUS
--- utils/consts.js
+++ utils/consts.js
@@ -2,6 +2,7 @@
 export const USER_TOKEN_DATA = "USER_TOKEN_DATA"
 export const AD_POP_UP_IMPRESSION_HISTORY = "AD_POP_UP_IMPRESSION_HISTORY"
 // 首页广告弹窗记录
+export const CONFIRM_AUTHORIZATION = "CONFIRM_AUTHORIZATION"
 
 export const PRIVACY_URL = `//www`
 export const USER_AGREEMENT_URL = `//www`
