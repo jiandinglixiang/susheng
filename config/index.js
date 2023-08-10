d6427df35ae866f4dedf5d37d0cf6b7ddf410f67
李* <jiandinglixiang@qq.com>
2023-08-10 23:19:14 +0800

    Revert "fix"
    This reverts commit fa9aca9b30c063c109ff4a2c3c3a6c609dcb1c3b.

--- config/index.js
+++ config/index.js
@@ -2,9 +2,7 @@ export const APP_ID = 21
 
 export const UniAppID = "__UNI__B693EAB"
 
-// export const BundleId = "com.gd.jjs"
-export const BundleId = "com.gds.jjs"
+export const BundleId = "com.gd.jjs"
 // 包名  com.gd.jjs
 // 云端证书
 // Abcd1234
-// ios包名 com.gds.jjs
